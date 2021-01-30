9631620d8beacc811e490bea3c2bbf45d91a0ee9
briankoehler <briankoehler@ufl.edu>
2021-01-30 12:35:32 -0500

    Implement reddit search

--- reddit.js
+++ reddit.js
@@ -1,32 +1,48 @@
 
 /***
- * Retrieve reddit post data from best post relating to search term
+ * Retrieve reddit post data from posts relating to search term.
+ * Checks "best" posts first, then searches term.
  */
-const getRedditPost = async (term) => {
+const getRedditPosts = async (term) => {
+	let posts = [];
 
 	/* Get best posts from Reddit  */
-	const res = await fetch('https://old.reddit.com/best.json');
-	const data = await res.json();
-	const posts = data['data']['children'];
+	const bestRes = await fetch('https://old.reddit.com/best.json');
+	const bestData = await bestRes.json();
+	const bestPosts = bestData['data']['children'];
 
-	/* Getting best post related to search term */
-	for (let post in posts) {
+	/* Get search posts from Reddit */
+	const searchRes = await fetch('https://old.reddit.com/search.json?sort=relevance&q=' + term);
+	const searchData = await searchRes.json();
+	const searchPosts = searchData['data']['children']
+
+	/* Concatentate all posts */
+	const allPosts = bestPosts.concat(searchPosts);
+
+	/* Getting posts related to search term */
+	for (let post in allPosts) {
 
 		// if (terms.some(substring => posts[post]['data']['title'].toLowerCase().includes(substring))) {
-		if (posts[post]['data']['title'].toLowerCase().includes(term)) {
+		if (allPosts[post]['data']['title'].toLowerCase().includes(term)) {
 
-			/* Return object with relevant post data */
-			return {
-				title: posts[post]['data']['title'],
-				author: posts[post]['data']['author'],
-				content: posts[post]['data']['selftext'],
-				upvotes: posts[post]['data']['ups'],
-				upvoteRatio: posts[post]['data']['upvote_ratio'],
-				url: 'https://reddit.com' + posts[post]['data']['permalink'],
-				utc: posts[post]['data']['created_utc'],
-				thumbnail: posts[post]['data']['thumbnail']
+			/* Push object with relevant post data */
+			let postData = {
+				title: allPosts[post]['data']['title'],
+				author: allPosts[post]['data']['author'],
+				content: allPosts[post]['data']['selftext'],
+				upvotes: allPosts[post]['data']['ups'],
+				upvoteRatio: allPosts[post]['data']['upvote_ratio'],
+				url: 'https://reddit.com' + allPosts[post]['data']['permalink'],
+				utc: allPosts[post]['data']['created_utc'],
+				thumbnail: allPosts[post]['data']['thumbnail']
 			}
-			
+			posts.push(postData);
+
+			/* Stopping if 5 posts found */
+			if (posts.length == 5) {
+				return posts;
+			}
 		}
 	}
+	return posts;
 }
