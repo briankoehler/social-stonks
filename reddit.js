b376d4a678b235b14c39fcd02b2acaccb504bf78
briankoehler <briankoehler@ufl.edu>
2021-01-30 13:37:46 -0500

    Implement subreddit data retrieval

--- reddit.js
+++ reddit.js
@@ -1,9 +1,10 @@
 
 /***
- * Retrieve reddit post data from posts relating to search term.
+ * Retrieve reddit post data from posts relating to search term
  * Checks "best" posts first, then searches term.
  */
-const getRedditPosts = async (term) => {
+const getRedditPost = async (term) => {
+
 	let posts = [];
 
 	/* Get best posts from Reddit  */
@@ -12,7 +13,7 @@ const getRedditPosts = async (term) => {
 	const bestPosts = bestData['data']['children'];
 
 	/* Get search posts from Reddit */
-	const searchRes = await fetch('https://old.reddit.com/search.json?sort=relevance&q=' + term);
+	const searchRes = await fetch(`https://old.reddit.com/search.json?sort=relevance&q=${term}`);
 	const searchData = await searchRes.json();
 	const searchPosts = searchData['data']['children']
 
@@ -30,9 +31,11 @@ const getRedditPosts = async (term) => {
 				title: allPosts[post]['data']['title'],
 				author: allPosts[post]['data']['author'],
 				content: allPosts[post]['data']['selftext'],
+				commentCount: allPosts[post]['data']['num_comments'],
 				upvotes: allPosts[post]['data']['ups'],
 				upvoteRatio: allPosts[post]['data']['upvote_ratio'],
-				url: 'https://reddit.com' + allPosts[post]['data']['permalink'],
+				subreddit: allPosts[post]['data']['subreddit'],
+				url: `https://reddit.com${allPosts[post]['data']['permalink']}`,
 				utc: allPosts[post]['data']['created_utc'],
 				thumbnail: allPosts[post]['data']['thumbnail']
 			}
@@ -42,7 +45,32 @@ const getRedditPosts = async (term) => {
 			if (posts.length == 5) {
 				return posts;
 			}
+
 		}
 	}
+
 	return posts;
+
+}
+
+
+/***
+ * Retrieve subreddit data given name found in URL
+ */
+const getSubredditInfo = (subName) => {
+	/* Get all subreddit data */
+	const res = await fetch(`https://old.reddit.com/r/${subName}/about.json`);
+	const data = await res.json();
+
+	/* Return relevant data as object */
+	return {
+		id: data['data']['id'],
+		displayName: data['data']['display_name'],
+		description: data['data']['public_description'],
+		icon: data['data']['icon_img'],
+		subscribers: data['data']['subscribers'],
+		url: `https://reddit.com${data['data']['url']}`,
+		matureContent: data['data']['over18'],
+		primaryColor: data['data']['primary_color']
+	}
 }
