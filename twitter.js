7ef11636318b3ccb42d7bb49f7329d1fe3a2063d
Sumanth Aluri <sumanth.phi2000@gmail.com>
2021-01-30 19:20:24 -0500

    fixint twitter

--- twitter.js
+++ twitter.js
@@ -1,9 +1,10 @@
 
 // let t = 'wallstreetbets'
-const getTwitter = (term) => {
-	let return_tweets = [];
+async function getTwitter (term) {
+    // let return_tweets = [];
+    let return_tweets = new Array();
 
-	fetch('https://whispering-wildwood-21421.herokuapp.com/https://api.twitter.com/2/tweets/search/recent?&query=' + term + '  is:verified lang:en&tweet.fields=public_metrics&user.fields=username,profile_image_url&expansions=author_id', {
+	const hold =  await fetch('https://whispering-wildwood-21421.herokuapp.com/https://api.twitter.com/2/tweets/search/recent?&query=' + term + '  is:verified lang:en&tweet.fields=public_metrics&user.fields=username,profile_image_url&expansions=author_id', {
     method: 'GET',
     headers: {
         'Content-Type': 'application/json',
@@ -13,46 +14,74 @@ const getTwitter = (term) => {
 		'Access-Control-Allow-Headers': 'Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With',
 		'Access-Control-Allow-Credentials': true
     }
-	}).then(function (response) {
-		// The API call was successful!
-		if (response.ok) {
-			return response.json();
-		} else {
-			return Promise.reject(response);
-		}
-	}).then(function (data) {
-		// This is the JSON from our response
-		const zip = (a, b) => a.map((k, i) => [k, b[i]]);
-		let queried_tweets = data.data;
-		let queried_users = data.includes.users;
-        // let zipped_tweets = (zip(queried_tweets,queried_users));
+    });
+    const data = await hold.json();
 
-		queried_tweets.forEach(tweet => {
+    
+    // .then(function (response) {
+	// 	// The API call was successful!
+	// 	if (response.ok) {
+	// 		return response.json();
+	// 	} else {
+	// 		return Promise.reject(response);
+	// 	}
+    // })
 
-            let author;
+    let queried_tweets = data.data;
+    let queried_users = data.includes.users;
+    console.log(queried_users);
+    
+
+    queried_tweets.forEach(tweet => {
+
+        let author;
+    
+        queried_users.forEach(person =>{
+            if( tweet.author_id === person.id){
+                author = person;
+            }
+        })
+
+
+    // const returntws = 
+    // .then(function (data) {
+	// 	// This is the JSON from our response
+	// 	// const zip = (a, b) => a.map((k, i) => [k, b[i]]);
+	// 	let queried_tweets = data.data;
+	// 	let queried_users = data.includes.users;
+    //     // let zipped_tweets = (zip(queried_tweets,queried_users));
         
-            queried_users.forEach(person =>{
-                if( tweet.author_id === person.id){
-                    author = person;
-                }
-            })
 
-			let tweetData = {
-				username: author.username,
-				profile_pic: author.profile_image_url,
-				text: tweet.text,
-				retweet_count: tweet.public_metrics.retweet_count,
-				reply_count: tweet.public_metrics.reply_count,
-				like_count: tweet.public_metrics.like_count,
-			}
-			return_tweets.push(tweetData);
-		});
-	}).catch(function (err) {
-		// There was an error
-		console.warn('Something went wrong.', err);
-	});
+		// queried_tweets.forEach(tweet => {
 
-	return return_tweets;
+        //     let author;
+        
+        //     queried_users.forEach(person =>{
+        //         if( tweet.author_id === person.id){
+        //             author = person;
+        //         }
+        //     })
+
+
+	// 		let tweetData = {
+	// 			username: author.username,
+	// 			profile_pic: author.profile_image_url,
+	// 			text: tweet.text,
+	// 			retweet_count: tweet.public_metrics.retweet_count,
+	// 			reply_count: tweet.public_metrics.reply_count,
+	// 			like_count: tweet.public_metrics.like_count,
+	// 		}
+	// 		return_tweets.push(tweetData);
+    //     });
+        
+        
+	// }).catch(function (err) {
+	// 	// There was an error
+	// 	console.warn('Something went wrong.', err);
+    // });
+
+    return return_tweets;
+
 }
 
 export { getTwitter };
